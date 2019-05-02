1c8ade7f509e34b8ff585da49cd8188cf941d89d
wentian wang <wangwentian0@hotmail.com>
2019-05-03 00:32:30 +0800

    增加一道题目hard的

---
+++ 二叉查找树纠错/solution.py
@@ -0,0 +1,60 @@
+"""
+等级： hard
+二叉查找树中有两个元素交换了位置，导致二叉查找树不再合规，找到这两个元素并且交换回来
+
+解法：
+
+中序遍历为 左根右，所以中序遍历一定是个升序序列，找到生序序列中的两个异常点，交换回来即可
+
+例如
+7 3 4 5 1
+正确应该是 1 3 4 5 7， 3 4 5为升序，7和1交换了位置导致bst不再合规
+"""
+import sys
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def __init__(self):
+        self.firstNode = None
+        self.secondNode = None
+        self.curNode = None
+
+    def fix(self, root):
+        cur = root
+        self.curNode = cur
+        self.mid_traversal(cur)
+        temp = self.firstNode.val
+        self.firstNode.val = self.secondNode.val
+        self.secondNode.val = temp
+        return root
+    def mid_traversal(self, root):
+        if not root:
+            return
+        self.mid_traversal(root.left)
+        if self.firstNode and self.curNode.val > root.val:
+            self.secondNode = root
+        if not self.firstNode and self.curNode.val > root.val:
+            self.firstNode = self.curNode
+        self.curNode = root
+        self.mid_traversal(root.right)
+    
+if __name__ == "__main__":
+    s = Solution()
+    n7 = TreeNode(7)
+    n3 = TreeNode(3)
+    root = TreeNode(4)
+    n5 = TreeNode(5)
+    n1 = TreeNode(1)
+    root.left = n3
+    n3.left = n7
+    n1.left = n5
+    root.right = n1
+
+    s.fix(root)
+    print(n7.val)
+    print(n1.val)
